e6544d025df9156a934ddb1ef97b75b6ffa5d5bc
Vladimir Guzman <guzman.vla@gmail.com>
2016-02-08 01:08:01 -0500

    remove unused code

--- app/models/space.rb
+++
@@ -1,5 +0,0 @@
-class Space < ActiveRecord::Base
- belongs_to :field
- attr_accessible :state, :pos_x, :pos_y
-
-end
--- db/migrate/20160207025623_create_fields.rb
+++
@@ -1,8 +0,0 @@
-class CreateFields < ActiveRecord::Migration
-  def change
-    create_table :fields do |t|
-      t.integer :high
-      t.integer :width
-    end
-  end
-end
